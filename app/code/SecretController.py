b4aae132600b96cc354a8a03d7fd152f8664c984
Christian Gleerup <christian.gleerup@gmail.com>
2022-08-07 17:53:10 +0200

    Added last controller methods and html

--- app/code/SecretController.py
+++ app/code/SecretController.py
@@ -6,8 +6,32 @@ app = Flask(__name__)
 @app.route('/')
 def EnterSecret():
     return render_template(
-        'enter-secret.html')
+        'enter-secret.html',
+        save_secret_url=url_for('SaveSecret'),
+        title='Secret sharing - enter your secret')
 
-@app.route('/save')
+@app.route('/save', methods = ['POST'])
 def SaveSecret():
-    return "world"
+    return redirect(
+        url_for(
+            'ConfirmSecretSaved', 
+            secretId = repo.Store(request.form['secret'])))
+
+@app.route('/saved/<uuid:secretId>')
+def ConfirmSecretSaved(secretId):
+    return render_template(
+        'confirm_secret_saved.html',
+        secret_url = url_for(
+            'ViewSecret',
+            secretId = secretId,
+            _external = True))
+
+@app.route('/view/secret/<uuid:secretId>')
+def ViewSecret(secretId):
+    if (repo.SecretExists(secretId)):
+        return render_template(
+            'view_secret.html',
+            secret=repo.Retrieve(secretId))
+    return render_template(
+        'no_such_secret.html',
+        url_for_new_secret=url_for('EnterSecret'))
