c5d9d76b61927bebe88f7c5d65313c2c8e919b0f
Christian Gleerup <christian.gleerup@gmail.com>
2022-08-14 19:15:24 +0200

    disabled failing test

--- app/test/test_SecretRepo.py
+++ app/test/test_SecretRepo.py
@@ -26,15 +26,6 @@ class test_SecretRepo(unittest.TestCase):
             Repo.Retrieve,
             secretId)
 
-    def test_what_happens_when_a_test_fails_for_github_actions(self):
-        self.assertTrue(
-            False)
-    
-if __name__ == '__main__':
-    repo = test_SecretRepo()
-    repo.test_repo_store_and_get_secret()
-    repo.test_repo_cannot_retrieve_non_stored_secret()
-    repo.test_repo_cannot_retrieve_more_than_once()
-    repo.what_happens_when_a_test_fails_for_github_actions()
-    print("Tests executed")
-
+    # def test_what_happens_when_a_test_fails_for_github_actions(self):
+    #     self.assertTrue(
+    #         False)
