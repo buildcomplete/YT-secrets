ec11c2d00a4ec5555db7f1aabc6eec4ba120efeb
Christian Gleerup <christian.gleerup@gmail.com>
2022-08-14 15:55:31 +0200

    Updated Dockerfile and added docker-compose application is now started instead of running tests by default

---
+++ deploy-to-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+eval $(ssh-agent) # Create agent and environment variables
+ssh-add ~/.ssh/secrets
+rsync -az --progress --stats --chown=www-data:www-data ./app root@142.93.137.143:/var/www/secret_app
