970d212f7fc50cfa1f2f03d62192aabe97007e90
Christian Gleerup <christian.gleerup@gmail.com>
2022-08-14 19:13:17 +0200

    est_what_happens_when_a_test_fails_for_github_actions

--- app/test/test_SecretRepo.py
+++ app/test/test_SecretRepo.py
@@ -25,11 +25,16 @@ class test_SecretRepo(unittest.TestCase):
             SecretExceptions.MissingSecretException,
             Repo.Retrieve,
             secretId)
+
+    def test_what_happens_when_a_test_fails_for_github_actions(self):
+        self.assertTrue(
+            False)
     
 if __name__ == '__main__':
     repo = test_SecretRepo()
     repo.test_repo_store_and_get_secret()
     repo.test_repo_cannot_retrieve_non_stored_secret()
     repo.test_repo_cannot_retrieve_more_than_once()
+    repo.what_happens_when_a_test_fails_for_github_actions()
     print("Tests executed")
 
