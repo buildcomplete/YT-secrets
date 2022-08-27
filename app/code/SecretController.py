eb33343501d1891e272fcd53067983bba4fd1e35
Christian Gleerup <christian.gleerup@gmail.com>
2022-08-27 12:50:22 +0200

    updated port from 80 to 5000 and added werkzeug proxyfix to controller

--- app/code/SecretController.py
+++ app/code/SecretController.py
@@ -1,7 +1,9 @@
 from flask import Flask, make_response, render_template, url_for, request, redirect
+from werkzeug.middleware.proxy_fix import ProxyFix
 import code.SecretRepo as repo
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 
 @app.route('/')
 def EnterSecret():
